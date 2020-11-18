1ef7d5a415ae3e765b714e1e2d615c3070b5c7a5
kimlimjustin <kimlimjustin@gmail.com>
2020-11-18 22:22:07 +0700

    Created Forms model

--- index/models.py
+++ index/models.py
@@ -4,3 +4,37 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser, models.Model):
     email = models.EmailField(unique = True)
+
+class Choices(models.Model):
+    choice = models.CharField(max_length=5000)
+    is_answer = models.BooleanField(default=None)
+
+class Questions(models.Model):
+    question = models.CharField(max_length= 10000)
+    question_type = models.CharField(max_length=20)
+    required = models.BooleanField(default= False)
+    answer_key = models.CharField(max_length = 5000, blank = True)
+    score = models.IntegerField(blank = True)
+    choices = models.ManyToManyField(Choices, related_name = "choices")
+
+class Answer(models.Model):
+    answer = models.CharField(max_length=5000)
+    answer_to = models.ForeignKey(Questions, on_delete = models.CASCADE ,related_name = "answer_to")
+
+class Form(models.Model):
+    code = models.CharField(max_length=30)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=10000, blank = True)
+    creator = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "creator")
+    background_color = models.CharField(max_length=20, default = "#d9efed")
+    text_color = models.CharField(max_length=20, default="#272124")
+    createdAt = models.DateTimeField(auto_now_add = True)
+    updatedAt = models.DateTimeField(auto_now = True)
+    questions = models.ManyToManyField(Questions, related_name = "questions")
+
+class Responses(models.Model):
+    response_code = models.CharField(max_length=20)
+    response_to = models.ForeignKey(Form, on_delete = models.CASCADE, related_name = "response_to")
+    responder_ip = models.CharField(max_length=30)
+    responder = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "responder", blank = True, null = True)
+    response = models.ManyToManyField(Answer, related_name = "response")
