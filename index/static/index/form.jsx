48f8a5afa285a4cfabc546aef909978c146134c4
kimlimjustin <kimlimjustin@gmail.com>
2020-11-20 21:59:18 +0700

    Added form title and description layout

--- index/static/index/form.jsx
+++ index/static/index/form.jsx
@@ -1,28 +1,12 @@
-const editData = e =>{
-    const el = e.target;
-    const input = document.createElement("input");
-    input.setAttribute("value", el.textContent);
-    input.setAttribute('class', el.className)
-    el.replaceWith(input);
-
-    const save = () => {
-        const previous = document.createElement(el.tagName.toLowerCase());
-        previous.onclick = editData;
-        previous.textContent = input.value;
-        previous.className = el.className
-        input.replaceWith(previous)
-
-    };
-
-    input.addEventListener("blur", save, {
-        once: true
-    })
-    input.focus();
+function textAreaAdjust(element) {
+    element.target.style.height = "auto";
+    element.target.style.height = (element.target.scrollHeight)+"px";
 }
-
 const Form = (code) => {
     const [formInfo, setFormInfo] = React.useState({});
     const [width, setWidth] = React.useState(0);
+    const [title, setTitle] = React.useState('');
+    const [description, setDescription] = React.useState('');
     
     React.useEffect(() => {
         const csrf = Cookies.get('csrftoken');
@@ -34,18 +18,14 @@ const Form = (code) => {
         .then(result => {
             setFormInfo(result)
             document.body.style.backgroundColor = result.background_color;
+            document.body.style.color = result.text_color;
+            setTitle(result.title)
         })
     }, [])
 
     React.useEffect(() => {
-        for (var child of document.querySelectorAll('[data-editable]')) {
-            child.onclick = editData;
-        }
-    }, [])
-
-    React.useEffect(() => {
-        if(formInfo.title) document.title = `${formInfo.title} - Google Form CLONE`
-    }, [formInfo.title])
+        if(title) document.title = `${title} - Google Form CLONE`
+    }, [title])
 
     React.useEffect(() => {
         setWidth(window.innerWidth);
@@ -60,7 +40,7 @@ const Form = (code) => {
                 <a href = "/">
                     <img src = "/static/Icon/icon.png" alt = "Google Forms Icon(CLONE)" className="navbar-icon form-icon" title = "Forms" />
                 </a>
-                <span class="nav-text nav-form-title" data-editable>{formInfo.title}</span>
+                <input class="nav-text nav-form-title edit-on-click" value  = {title} type="text" onChange = {({target: {value}}) => setTitle(value)} placeholder = "Form title" />
                 {width > 768?
                 <div className="float-right">
                     <img src="/static/Icon/theme.png" alt="Theme icon" title = "Customize theme" className="nav-form-menu-icon" />
@@ -74,6 +54,15 @@ const Form = (code) => {
                     <img src="/static/Icon/send.png" alt ="Send icon" title = "Send" className="nav-form-menu-icon" />
                     <img src="/static/Icon/options.png" alt="Options icon" title = "More" className="nav-form-menu-icon" />
                 </div>}
+            </div>
+            <div className="container">
+                <div className="margin-top-bottom box question-box">
+                    <div className="form-title-box">
+                        <input type = "text" className="form-title edit-on-click" value = {title} onChange = {({target: {value}}) => setTitle(value)} placeholder = "Form title" />
+                    </div>
+                    <textarea className="form-description edit-on-click" rows="1" placeholder="Form description" value = {description} 
+                    onChange = {({target: {value}}) => setDescription(value)} onKeyUp = {textAreaAdjust}></textarea>
+                </div>
             </div>
         </div>
     )
