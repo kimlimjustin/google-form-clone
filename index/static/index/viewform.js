61bf1a8af2427e1b4573af1f6b3ddcf3cd73e52e
kimlimjustin <kimlimjustin@gmail.com>
2020-11-28 19:46:55 +0700

    Fixed required on checkbox question

--- index/static/index/viewform.js
+++ index/static/index/viewform.js
@@ -12,4 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
             tx.style.height = (10 + tx.scrollHeight)+"px";
         })
     })
+    document.querySelectorAll('input[type="checkbox"]').forEach(element => {
+        document.getElementsByName(element.name).forEach(checkbox => {
+            checkbox.addEventListener("input", function(){
+                let totalChecked = 0
+                document.getElementsByName(element.name).forEach(checkbox => {
+                    if(checkbox.checked) totalChecked++;
+                })
+                if(totalChecked > 0){
+                    document.getElementsByName(element.name).forEach(checkbox => {
+                        checkbox.removeAttribute("required")
+                    })
+                }else{
+                    document.getElementsByName(element.name).forEach(checkbox => {
+                        checkbox.setAttribute("required", '')
+                    })
+                }
+            })
+        })
+    })
 })
