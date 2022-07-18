2d95581ad77a9559f3226e4cc998138d5370e3f1
BossGamerDEV <102386419+BossGamerDEV@users.noreply.github.com>
2022-07-18 19:57:11 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
